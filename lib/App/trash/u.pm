8413e5e3e2e6a2a817adfdc05d22a84a32b200d1
perlancar (@netbook-zenbook-ux305) <perlancar@gmail.com>
2016-12-27 22:56:17 +0700

    Initial commit

---
+++ lib/App/trash/u.pm
@@ -0,0 +1,16 @@
+package App::trash::u;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: Trash files, with undo/redo capability
+
+=head1 DESCRIPTION
+
+See included script L<trash-u>.
+
+
+=head1 SEE ALSO
+
+L<File::Trash::Undoable>
